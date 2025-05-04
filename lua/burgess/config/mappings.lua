b89594032ec57257e2d37b839cdf35adcc0773a4
Adrien Burgess <a.burke.burgess@gmail.com>
2025-05-04 10:49:16 -0300

    adding changes

--- lua/burgess/config/mappings.lua
+++ lua/burgess/config/mappings.lua
@@ -5,18 +5,18 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 local function map(mode, lhs, rhs)
-    vim.keymap.set(mode, lhs, rhs, { silent = true })
+	vim.keymap.set(mode, lhs, rhs, { silent = true })
 end
 
 -- Clipboard
 -- Copy to system clipboard
-map("n", "<leader>y", "\"y")
-map("n", "<leader>yy", "\"yy")
+map("n", "<leader>y", '"y')
+map("n", "<leader>yy", '"yy')
 -- Cut to system clipboard
 map("n", "<leader>v", "\v")
-map("n", "<leader>vv", "\"vv")
+map("n", "<leader>vv", '"vv')
 -- Paste from system clipboard
-map("n", "<leader>p", "\"p")
+map("n", "<leader>p", '"p')
 
 -- Split windows
 map("n", "<leader>j", "<CMD>split<CR>")
@@ -27,3 +27,7 @@ map("n", "<C-Left>", "<C-w><")
 map("n", "<C-Right>", "<C-w>>")
 map("n", "<C-Up>", "<C-w>+")
 map("n", "<C-Down>", "<C-w>-")
+
+-- Tabs
+map("n", "<leader>0", "<CMD>tabprev<CR>")
+map("n", "<leader>9", "<CMD>tabnext<CR>")
